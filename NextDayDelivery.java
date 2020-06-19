59c64327362c26cdcf216039181f5c73bea74f3e
ngshenmeng2401 <55821381+ngshenmeng2401@users.noreply.github.com>
2020-06-19 18:28:09 +0800

    Add files via upload

--- NextDayDelivery.java
+++ NextDayDelivery.java
@@ -15,11 +15,13 @@ public class NextDayDelivery extends JFrame implements ActionListener{
 
     private JPanel contentPane;
     private JTable table;
-    private static JRadioButton rb1,rb2,rb3,rb4,rb5,rb6,rb7;
+    private JRadioButton rb1,rb2,rb3,rb4,rb5,rb6,rb7;
     private JTextField textField;
     private JButton button1,button2;
+    private double weight,charge1;
 
     public NextDayDelivery() {
+
 
         addWindowListener(new WindowAdapter() {
             @Override
@@ -78,7 +80,12 @@ public class NextDayDelivery extends JFrame implements ActionListener{
         contentPane.add(rb6);
         contentPane.add(rb7);
 
-        rb1.addActionListener(this);
+        rb3.addActionListener(this);
+        rb4.addActionListener(this);
+        rb5.addActionListener(this);
+        rb6.addActionListener(this);
+        rb7.addActionListener(this);
+
 
         textField = new JTextField();
         textField.setBounds(150, 310, 165, 25);
@@ -147,7 +154,98 @@ public class NextDayDelivery extends JFrame implements ActionListener{
 
         if (e.getSource()==button1){
 
-            new NDDbutton();
+            String text = textField.getText();
+            weight= Double.parseDouble(text);
+            if (rb3.isSelected()){
+                if (weight <= 2000) {
+                    if (weight <= 2000) {
+                        charge1 = 4.90 + (weight - 500) / 250 * 0.80;
+                    }
+                    if (weight <= 500) {
+                        charge1 = 4.90;
+                    }
+                }if (weight > 2000) {
+                    if (weight <= 2500) {
+                        charge1 = 10.50;
+                    }
+                    if (weight > 2500) {
+                        charge1 = 10.50 + (weight - 2500) / 500 * 0.50;
+                    }
+                }
+
+            }if (rb4.isSelected()){
+                if (weight <= 2000) {
+                    if (weight <= 2000) {
+                        charge1 = 5.40 + (weight - 500) / 250 * 1.00;
+                    }
+                    if (weight <= 500) {
+                        charge1 = 5.40;
+                    }
+                }
+                if (weight > 2000) {
+                    if (weight <= 2500) {
+                        charge1 = 16.00;
+                    }
+                    if (weight > 2500) {
+                        charge1 = 16.00 + (weight - 2500) / 500 * 2.00;
+                    }
+                }
+
+            }if (rb5.isSelected()){
+                if (weight <= 2000) {
+                    if (weight <= 2000) {
+                        charge1 = 6.90 + (weight - 500) / 250 * 1.50;
+                    }
+                    if (weight <= 500) {
+                        charge1 = 6.90;
+                    }
+                }
+                if (weight > 2000) {
+                    if (weight <= 2500) {
+                        charge1 = 21.00;
+                    }
+                    if (weight > 2500) {
+                        charge1 = 21.00 + (weight - 2500) / 500 * 3.00;
+                    }
+                }
+
+            }if (rb6.isSelected()){
+                if (weight <= 2000) {
+                    if (weight <= 2000) {
+                        charge1 = 7.40 + (weight - 500) / 250 * 1.50;
+                    }
+                    if (weight <= 500) {
+                        charge1 = 7.40;
+                    }
+                }
+                if (weight > 2000) {
+                    if (weight <= 2500) {
+                        charge1 = 26.00;
+                    }
+                    if (weight > 2500) {
+                        charge1 = 26.00 + (weight - 2500) / 500 * 3.50;
+                    }
+                }
+
+            }if (rb7.isSelected()){
+                if (weight <= 2000) {
+                    if (weight <= 2000) {
+                        charge1 = 7.90 + (weight - 500) / 250 * 2.00;
+                    }
+                    if (weight <= 500) {
+                        charge1 = 7.90;
+                    }
+                }
+                if (weight > 2000) {
+                    if (weight <= 2500) {
+                        charge1 = 31.00;
+                    }
+                    if (weight > 2500) {
+                        charge1 = 31.00 + (weight - 2500) / 500 * 4.00;
+                    }
+                }
+            }
+
             JOptionPane.showMessageDialog(this, "Data Saved");
 
         }if (e.getSource()==button2){
@@ -155,5 +253,9 @@ public class NextDayDelivery extends JFrame implements ActionListener{
             setVisible(false);
         }
 
+    }
+
+    public double getCharge1() {
+        return charge1;
     }
 }
